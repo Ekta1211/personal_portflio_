74454df10a503eaa46fc0e7a611b445e82081dbe
Ekta <ekta16567@gmail.com>
2023-09-06 23:26:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing the animation in after name
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Python Programmer"],
+    typeSpeed: 90,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true
+});
